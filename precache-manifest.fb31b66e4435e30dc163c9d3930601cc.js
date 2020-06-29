908e88a1fa52e2624606834bab9eb87d743e8c74
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-06-29 20:40:12 +0530

    Updates

---
+++ precache-manifest.fb31b66e4435e30dc163c9d3930601cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cef824be38d10cc2bc4cb4b6b11647",
+    "url": "/CryptoNews/index.html"
+  },
+  {
+    "revision": "136c18d3894cfcc673e3",
+    "url": "/CryptoNews/static/css/main.37d52b54.chunk.css"
+  },
+  {
+    "revision": "bb5c615881605c028ab4",
+    "url": "/CryptoNews/static/js/2.fcaf9218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CryptoNews/static/js/2.fcaf9218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136c18d3894cfcc673e3",
+    "url": "/CryptoNews/static/js/main.79fd7bfe.chunk.js"
+  },
+  {
+    "revision": "727dc2e025432dbcafc8",
+    "url": "/CryptoNews/static/js/runtime-main.0c915d72.js"
+  }
+]);
